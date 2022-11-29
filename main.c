57cc2128fb70d41179852ae5118a63fa7d832392
yeojinseo <113421483+yeojinseo@users.noreply.github.com>
2022-11-29 19:58:56 +0900

    02

--- main.c
+++ main.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-struct student {
-	int 	ID;
-	char 	name[10];
-	float 	grade;
+struct point {
+	int x;
+	int y;
 };
 
 
 int main(int argc, char *argv[]) {
-	struct student student1 = {100000, "ren", 4.0};
 	
-	student1.ID = 123456;
-	strcpy(student1.name, "Yeojin");
-	student1.grade = 4.3;
+	struct point p1, p2;
+	int xdiff, ydiff;
+	double dist;
 	
-	printf("ID : %d\n", student1.ID);
-	printf("name : %s\n", student1.name);
-	printf("grade : %f\n", student1.grade);
+	printf("input p1 coordinate (x y) : ");
+	scanf("%d %d", &p1.x, &p1.y);
 
+	printf("input p2 coordinate (x y) : ");
+	scanf("%d %d", &p2.x, &p2.y);
+	
+	//calculate xdiff
+	xdiff = p2.x - p1.x;
+	
+	//calculate ydiff
+	ydiff = p2.y - p1.y;
+	
+	//피타고라스 정리 sqrt(xdiff^2 + ydiff^2) -> dist 
+	dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+	
+	printf("distance : %lf\n", dist);
 	
 	return 0;
 }
